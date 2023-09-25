0daa39bf80a3fb0572be1ab2a9458e9b8a832451
Andrew Pope <apope37@gatech.edu>
2023-09-25 08:31:08 -0600

    add ability to not ensure downstream models ran

--- dbt_cloud_plugin/helpers.py
+++ dbt_cloud_plugin/helpers.py
@@ -1,12 +1,18 @@
 from .operators.dbt_cloud_check_model_result_operator import DbtCloudCheckModelResultOperator
 
 
-def generate_dbt_model_dependency(dbt_job_task, downstream_task, dependent_models):
+def generate_dbt_model_dependency(dbt_job_task, downstream_tasks, dependent_models, ensure_models_ran=True):
+    if isinstance(downstream_tasks, list):
+        task_id = f'check_dbt_model_results__{dbt_job_task.task_id}__{len(downstream_tasks)}_downstream'
+    else:
+        task_id = f'check_dbt_model_results__{dbt_job_task.task_id}__{downstream_task.task_id}'
+    
     check_dbt_model_results = DbtCloudCheckModelResultOperator(
-        task_id=f'check_dbt_model_results__{dbt_job_task.task_id}__{downstream_task.task_id}',
+        task_id=task_id,
         dbt_cloud_conn_id=dbt_job_task.dbt_cloud_conn_id,
         dbt_cloud_run_id=f'{{{{ ti.xcom_pull(task_ids="{dbt_job_task.task_id}", key="dbt_cloud_run_id") }}}}',
         model_names=dependent_models,
+        ensure_models_ran=ensure_models_ran,
         trigger_rule='all_done',
         retries=0
     )
